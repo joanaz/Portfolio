4d5830dd9dba6b6bb90b32d7e9384d599852608d
joanaz <rz258@cornell.edu>
2016-04-11 18:17:54 -0400

    commit cert

--- server/main.js
+++ server/main.js
@@ -1,4 +1,11 @@
 'use strict';
+var fs = require('fs');
+
+var options = {
+  key: fs.readFileSync('resources/domain.key.pem'),
+  cert: fs.readFileSync('resources/bundle.pem')
+};
+
 var chalk = require('chalk');
 
 // Requires in ./db/index.js -- which returns a promise that represents
@@ -6,25 +13,38 @@ var chalk = require('chalk');
 var startDb = require('./db');
 
 // Create a node server instance! cOoL!
-var server = require('http').createServer();
+// var server = require('http').createServer();
 
-var createApplication = function () {
-    var app = require('./app');
-    server.on('request', app); // Attach the Express application.
-    require('./io')(server);   // Attach socket.io.
+var secureServer = require('https').createServer(options)
+  // , function(req, res) {
+  //   res.writeHead(200);
+  //   res.end('hello world\n');
+  // })
+
+var createApplication = function() {
+  var app = require('./app');
+  // server.on('request', app); // Attach the Express application.
+  secureServer.on('request', app)
+    // require('./io')(server); // Attach socket.io.
+  require('./io')(secureServer);
 };
 
-var startServer = function () {
+var startServer = function() {
 
-    var PORT = process.env.PORT || 1337;
+  // var PORT = process.env.PORT || 8000;
 
-    server.listen(PORT, function () {
-        console.log(chalk.blue('Server started on port', chalk.magenta(PORT)));
-    });
+  // server.listen(PORT, function() {
+  //   console.log(chalk.blue('Server started on port', chalk.magenta(PORT)));
+  // });
+
+  var SECURE_PORT = process.env.PORT || 8443;
+  secureServer.listen(SECURE_PORT, function() {
+    console.log(chalk.blue('Server started on port', chalk.magenta(SECURE_PORT)));
+  });
 
 };
 
-startDb.then(createApplication).then(startServer).catch(function (err) {
-    console.error(chalk.red(err.stack));
-    process.kill(1);
+startDb.then(createApplication).then(startServer).catch(function(err) {
+  console.error(chalk.red(err.stack));
+  process.kill(1);
 });
